f7aafd5ebb88f0cf3bafa01b7e10e63bac833db8
Arnaud BODEL <53226044+ArnaudBDL@users.noreply.github.com>
2025-01-05 05:10:49 +0100

    Commit without message

--- assets/scripts/generated.js
+++ assets/scripts/generated.js
@@ -1,4 +1,4 @@
-// Last gen: 2025-01-01 16:58:06
+// Last gen: 2025-01-03 22:32:57
 export const PREFIX = 'ux';
 export const SEARCH_BOX_TYPES = ['basic','modal','full-page'];
 export const SEARCH_BOX_SHORTCUT = 'K';
