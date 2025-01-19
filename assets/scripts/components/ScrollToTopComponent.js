37b19ad12dbc64b97027411006a0e201b96cfe9e
Arnaud BODEL <53226044+ArnaudBDL@users.noreply.github.com>
2025-01-19 12:27:35 +0100

    Commit without message

--- assets/scripts/components/ScrollToTopComponent.js
+++ assets/scripts/components/ScrollToTopComponent.js
@@ -21,49 +21,55 @@
 // ************************************************************************** //
 "use strict";
 
-import AbstractComponent from './../abstracts/AbstractComponent';
-import { getPrefix } from "../utils/prefix";
+// import AbstractComponent from './../abstracts/AbstractComponent';
+// import { getPrefix } from "../utils/prefix";
 
-/** @var string Component classname */
-const NAME = 'scroll-to-top';
+// /** @var string Component classname */
+// const NAME = 'scroll-to-top';
 
-/** @var string Components prefix */
-const PREFIX = getPrefix();
+// /** @var string Components prefix */
+// const PREFIX = getPrefix();
 
-/** @var string Component selector */
-const SELECTOR = `[rel=js-${PREFIX}${NAME}]`;
+// /** @var string Component selector */
+// const SELECTOR = `[rel=js-${PREFIX}${NAME}]`;
 
 
-/** @var number Set the "top" position */
-const TOP_AT = 0;
+// /** @var number Set the "top" position */
+// const TOP_AT = 0;
 
-/** @var number Trigger point to show/hide the button */
-const TOGGLE_AT = 200;
+// /** @var number Trigger point to show/hide the button */
+// const TOGGLE_AT = 200;
 
-export default class ScrollToTopComponent extends AbstractComponent
-{
-    _onInit() 
-    {
-        window.onload = x => this.#scroll();
-        window.onscroll = x => this.#scroll();
-        this.on('click');
-    }
+// export default class ScrollToTopComponent extends AbstractComponent
+// {
+//     _onInit() 
+//     {
+//         window.onload = x => this.#scroll();
+//         window.onscroll = x => this.#scroll();
+//         this.on('click');
+//     }
 
-    #scroll()
-    {
-        const toggleAt   = this.node.dataset.toggleAt ?? TOGGLE_AT;
-        const toggleShow = document.body.scrollTop > toggleAt || document.documentElement.scrollTop > toggleAt;
+//     #scroll()
+//     {
+//         const toggleAt   = this.node.dataset.toggleAt ?? TOGGLE_AT;
+//         const toggleShow = document.body.scrollTop > toggleAt || document.documentElement.scrollTop > toggleAt;
 
-        this.node.classList.toggle('show', toggleShow);
-    }
+//         this.node.classList.toggle('show', toggleShow);
+//     }
 
-    _onClickAlways()
-    {
-        const topAt = this.node.dataset.topAt ?? TOP_AT;
+//     _onClickAlways()
+//     {
+//         const topAt = this.node.dataset.topAt ?? TOP_AT;
 
-        document.body.scrollTop            = topAt;
-        document.documentElement.scrollTop = topAt;
-    }
+//         document.body.scrollTop            = topAt;
+//         document.documentElement.scrollTop = topAt;
+//     }
+// }
+
+// document.querySelectorAll(SELECTOR).forEach(node => new ScrollToTopComponent(node));
+
+
+
+export default function ScrollToTopComponent() {
+    alert('Plop');
 }
-
-document.querySelectorAll(SELECTOR).forEach(node => new ScrollToTopComponent(node));
